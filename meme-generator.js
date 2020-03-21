a5f5c1a9494ce6568c40628b166544f7cb9107f9
Anjelica Silva <anjelicacsilva@gmail.com>
2020-03-21 18:27:36 +0000

    Create basic setup with meme generator and initialization functions

---
+++ meme-generator.js
@@ -0,0 +1,14 @@
+let topTextInput, bottomTextInput, imageInput, generateMemeBtn, canvas, ctx
+
+// Create a function that generates a meme
+function generateMeme () {
+
+}
+
+
+// Create an initialization function
+function init () {
+
+}
+
+init();
